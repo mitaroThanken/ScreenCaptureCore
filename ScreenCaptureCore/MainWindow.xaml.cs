f9b26f57e1d50acc57e621fba86fbeb338e90acf
YAMAMI Taro <mitarothanken@gmail.com>
2020-08-01 15:34:40 +0900

    MainWindow の内容をサンプルのものに更新

--- ScreenCaptureCore/MainWindow.xaml.cs
+++ ScreenCaptureCore/MainWindow.xaml.cs
@@ -1,17 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using CaptureSampleCore;
+using Composition.WindowsRuntimeHelpers;
+using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
-using System.Text;
+using System.Numerics;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Interop;
+using Windows.Foundation.Metadata;
+using Windows.Graphics.Capture;
+using Windows.UI.Composition;
 
 namespace ScreenCaptureCore
 {
@@ -20,9 +20,204 @@ namespace ScreenCaptureCore
     /// </summary>
     public partial class MainWindow : Window
     {
+        private IntPtr hwnd;
+        private Compositor compositor;
+        private CompositionTarget target;
+        private ContainerVisual root;
+
+        private BasicSampleApplication sample;
+        private ObservableCollection<Process> processes;
+        private ObservableCollection<MonitorInfo> monitors;
+
         public MainWindow()
         {
             InitializeComponent();
+
+#if DEBUG
+            var picker = new GraphicsCapturePicker();
+#endif
+        }
+
+        private async void PickerButton_Click(object sender, RoutedEventArgs e)
+        {
+            StopCapture();
+            WindowComboBox.SelectedIndex = -1;
+            MonitorComboBox.SelectedIndex = -1;
+            await StartPickerCaptureAsync();
+        }
+
+        private void PrimaryMonitorButton_Click(object sender, RoutedEventArgs e)
+        {
+            StopCapture();
+            WindowComboBox.SelectedIndex = -1;
+            MonitorComboBox.SelectedIndex = -1;
+            StartPrimaryMonitorCapture();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var interopWindow = new WindowInteropHelper(this);
+            hwnd = interopWindow.Handle;
+
+            var presentationSource = PresentationSource.FromVisual(this);
+            double dpiX = 1.0;
+            double dpiY = 1.0;
+            if (presentationSource != null)
+            {
+                dpiX = presentationSource.CompositionTarget.TransformToDevice.M11;
+                dpiY = presentationSource.CompositionTarget.TransformToDevice.M22;
+            }
+            var controlsWidth = (float)(ControlsGrid.ActualWidth * dpiX);
+
+            InitComposition(controlsWidth);
+            InitWindowList();
+            InitMonitorList();
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            StopCapture();
+            WindowComboBox.SelectedIndex = -1;
+            MonitorComboBox.SelectedIndex = -1;
+        }
+
+        private void WindowComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var comboBox = (ComboBox)sender;
+            var process = (Process)comboBox.SelectedItem;
+
+            if (process != null)
+            {
+                StopCapture();
+                MonitorComboBox.SelectedIndex = -1;
+                var hwnd = process.MainWindowHandle;
+                try
+                {
+                    StartHwndCapture(hwnd);
+                }
+                catch (Exception)
+                {
+                    Debug.WriteLine($"Hwnd 0x{hwnd.ToInt32():X8} is not valid for capture!");
+                    processes.Remove(process);
+                    comboBox.SelectedIndex = -1;
+                }
+            }
+        }
+
+        private void MonitorComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var comboBox = (ComboBox)sender;
+            var monitor = (MonitorInfo)comboBox.SelectedItem;
+
+            if (monitor != null)
+            {
+                StopCapture();
+                WindowComboBox.SelectedIndex = -1;
+                var hmon = monitor.Hmon;
+                try
+                {
+                    StartHmonCapture(hmon);
+                }
+                catch (Exception)
+                {
+                    Debug.WriteLine($"Hmon 0x{hmon.ToInt32():X8} is not valid for capture!");
+                    monitors.Remove(monitor);
+                    comboBox.SelectedIndex = -1;
+                }
+            }
+        }
+
+        private void InitComposition(float controlsWidth)
+        {
+            // Create the compositor.
+            compositor = new Compositor();
+
+            // Create a target for the window.
+            target = compositor.CreateDesktopWindowTarget(hwnd, true);
+
+            // Attach the root visual.
+            root = compositor.CreateContainerVisual();
+            root.RelativeSizeAdjustment = Vector2.One;
+            root.Size = new Vector2(-controlsWidth, 0);
+            root.Offset = new Vector3(controlsWidth, 0, 0);
+            target.Root = root;
+
+            // Setup the rest of the sample application.
+            sample = new BasicSampleApplication(compositor);
+            root.Children.InsertAtTop(sample.Visual);
+        }
+
+        private void InitWindowList()
+        {
+            if (ApiInformation.IsApiContractPresent(typeof(Windows.Foundation.UniversalApiContract).FullName, 8))
+            {
+                var processesWithWindows = from p in Process.GetProcesses()
+                                           where !string.IsNullOrWhiteSpace(p.MainWindowTitle) && WindowEnumerationHelper.IsWindowValidForCapture(p.MainWindowHandle)
+                                           select p;
+                processes = new ObservableCollection<Process>(processesWithWindows);
+                WindowComboBox.ItemsSource = processes;
+            }
+            else
+            {
+                WindowComboBox.IsEnabled = false;
+            }
+        }
+
+        private void InitMonitorList()
+        {
+            if (ApiInformation.IsApiContractPresent(typeof(Windows.Foundation.UniversalApiContract).FullName, 8))
+            {
+                monitors = new ObservableCollection<MonitorInfo>(MonitorEnumerationHelper.GetMonitors());
+                MonitorComboBox.ItemsSource = monitors;
+            }
+            else
+            {
+                MonitorComboBox.IsEnabled = false;
+                PrimaryMonitorButton.IsEnabled = false;
+            }
+        }
+
+        private async Task StartPickerCaptureAsync()
+        {
+            var picker = new GraphicsCapturePicker();
+            picker.SetWindow(hwnd);
+            GraphicsCaptureItem item = await picker.PickSingleItemAsync();
+
+            if (item != null)
+            {
+                sample.StartCaptureFromItem(item);
+            }
+        }
+
+        private void StartHwndCapture(IntPtr hwnd)
+        {
+            GraphicsCaptureItem item = CaptureHelper.CreateItemForWindow(hwnd);
+            if (item != null)
+            {
+                sample.StartCaptureFromItem(item);
+            }
+        }
+
+        private void StartHmonCapture(IntPtr hmon)
+        {
+            GraphicsCaptureItem item = CaptureHelper.CreateItemForMonitor(hmon);
+            if (item != null)
+            {
+                sample.StartCaptureFromItem(item);
+            }
+        }
+
+        private void StartPrimaryMonitorCapture()
+        {
+            MonitorInfo monitor = (from m in MonitorEnumerationHelper.GetMonitors()
+                                   where m.IsPrimary
+                                   select m).First();
+            StartHmonCapture(monitor.Hmon);
+        }
+
+        private void StopCapture()
+        {
+            sample.StopCapture();
         }
     }
 }
